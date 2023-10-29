f491cd1051d4421b9f9fb490342c191111b624bc
nazimfeni <mdnazimuddin77@gmail.com>
2023-10-29 16:16:07 +0600

    upadeted

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crud_api/screens/add_new_product_screen.dart';
 import 'package:crud_api/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 
@@ -7,18 +8,26 @@ class ProductListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Product List'),
-        ),
-        body: ListView.separated(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-
-            return const ProductItem();
-          },
-            separatorBuilder: (_, __) => Divider(),
-            ),);
+      appBar: AppBar(
+        title: Text('Product List'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const AddNewProductScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const ProductItem();
+        },
+        separatorBuilder: (_, __) => Divider(),
+      ),
+    );
   }
 }
-
-
