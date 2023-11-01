17beda1e80c39305f62586c9c58956e405849abf
nazimfeni <mdnazimuddin77@gmail.com>
2023-11-01 17:00:12 +0600

    upadeted

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,9 +1,55 @@
+import 'dart:convert';
+
 import 'package:crud_api/screens/add_new_product_screen.dart';
 import 'package:crud_api/widgets/product_item.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-class ProductListScreen extends StatelessWidget {
+class ProductListScreen extends StatefulWidget {
   const ProductListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductListScreen> createState() => _ProductListScreenState();
+}
+
+class _ProductListScreenState extends State<ProductListScreen> {
+  List<Product> productList = [];
+
+  @override
+  void initState() {
+    getProductList();
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void getProductList() async {
+    Response response = await get(
+      Uri.parse('https://crud.teamrabbil.com/api/v1/ReadProduct'),
+    );
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = jsonDecode(response.body);
+      if (responseData['status'] == 'success') {
+        for (Map<String, dynamic> productJson in responseData['data']) {
+          productList.add(Product(
+              productJson['_id'],
+              productJson['ProductName'],
+              productJson['ProductCode'],
+              productJson['Img'],
+              productJson['UnitPrice'],
+              productJson['Qty'],
+              productJson['TotalPrice']));
+        }
+      }
+    }
+
+    print(productList.length);
+    setState(() {
+
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +68,25 @@ class ProductListScreen extends StatelessWidget {
         child: Icon(Icons.add),
       ),
       body: ListView.separated(
-        itemCount: 10,
+        itemCount: productList.length,
         itemBuilder: (context, index) {
-          return const ProductItem();
+          return ProductItem();
         },
         separatorBuilder: (_, __) => Divider(),
       ),
     );
   }
+}
+
+class Product {
+  final String id;
+  final String productName;
+  final String productCode;
+  final String image;
+  final String unitPrice;
+  final String quantity;
+  final String totalPrice;
+
+  Product(this.id, this.productName, this.productCode, this.image,
+      this.unitPrice, this.quantity, this.totalPrice);
 }
